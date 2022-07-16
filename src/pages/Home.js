22791f94aa584abfb60892cc54de32544b0740bc
mahabur  rahman <annur4395@gmail.com>
2022-07-16 20:59:58 +0600

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Annoucement from "../components/Annoucement";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Annoucement />
+    </>
+  );
+};
+
+export default Home;
