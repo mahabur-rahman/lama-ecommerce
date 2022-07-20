838bebad8531394705c5f04dec0efa976bc3b928
mahabur-rahman <annur4395@gmail.com>
2022-07-20 11:13:41 +0600

    login route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -9,7 +9,10 @@ router.post("/register-user", async (req, res) => {
     username: req.body.username,
     email: req.body.email,
     // password: req.body.password,
-    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET),
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString(),
   });
 
   // save on db
@@ -22,6 +25,35 @@ router.post("/register-user", async (req, res) => {
 });
 
 //#######  LOGIN USER #######
+router.post("/login-user", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ username: req.body.username });
+    // console.log(user);
+
+    if (!user) {
+      return res.status(401).json("user not found !");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SECRET
+    );
+
+    // console.log(hashedPassword);
+
+    // original pass
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8); // pass : 123456
+
+    // console.log("original pass : ", originalPassword);
+    if (originalPassword !== req.body.password) {
+      return res.status(401).json("Incorrect Password");
+    }
+
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 // exports
 module.exports = router;
