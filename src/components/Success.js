cd7810ef5e8c15c0726a2f2b10c56835c57c360f
mahabur  rahman <annur4395@gmail.com>
2022-07-24 21:03:47 +0600

    stripe apply for checkout button

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const SuccessPayment = () => {
+  const location = useLocation();
+  console.log(location);
+
   return (
     <>
       <h1> payment Success</h1>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,14 @@
+import React, { useState, useEffect } from "react";
+
 import { Add, Remove } from "@material-ui/icons";
 import styled from "styled-components";
 import Announcement from "../components/Annoucement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import StripeCheckout from "react-stripe-checkout";
+import { userRequest } from "../publicMethods";
 
 const Container = styled.div``;
 
@@ -147,9 +151,41 @@ const Button = styled.button`
 `;
 
 const Cart = () => {
+  const history = useHistory();
+  // publishable key from (stripe.com)
+  const KEY = process.env.REACT_APP_STRIPE;
+
   const cart = useSelector((state) => state.cart);
+  const [stripeToken, setStripeToken] = useState(null);
 
   // console.log(cart);
+
+  // onToken func
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+
+  // console.log(stripeToken);
+
+  //  ####### api call  #######
+  useEffect(() => {
+    const makeRequest = async (req, res) => {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+
+        history.push("/success", {
+          stripeData: res.data,
+          products: cart,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [cart, history, stripeToken]);
 
   return (
     <Container>
@@ -220,7 +256,18 @@ const Cart = () => {
               <SummaryItemText>Total</SummaryItemText>
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
-            <Button>CHECKOUT NOW</Button>
+            <StripeCheckout
+              name="mahabur shop"
+              image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`Your total is : ${cart.total}`}
+              amount={cart.total * 100}
+              token={onToken}
+              stripeKey={KEY}
+            >
+              <Button>CHECKOUT NOW</Button>
+            </StripeCheckout>
           </Summary>
         </Bottom>
       </Wrapper>
