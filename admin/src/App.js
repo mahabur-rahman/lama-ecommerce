36e925781b43d1f903638532c1bf4a76759d24e2
mahabur-rahman <annur4395@gmail.com>
2022-07-25 15:48:33 +0600

    topbar design end

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,5 +1,15 @@
+// global style
+import "./index.scss";
+
+// components
+import Topbar from "./components/Topbar";
+
 function App() {
-  return <h1>app</h1>;
+  return (
+    <h1>
+      <Topbar />
+    </h1>
+  );
 }
 
 export default App;
