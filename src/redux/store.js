613af81ba39952dfe8d4b9329ef7b6eabc6da6a2
mahabur-rahman <annur4395@gmail.com>
2022-07-25 13:10:33 +0600

    redux persist data and protect routing

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
+// export const store = configureStore({
+//   reducer: {
+//     cart: cartReducer,
+//     user: userReducer,
+//   },
+// });
+
+// ######### redux persist use #########
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// exports
+export let persistor = persistStore(store);
