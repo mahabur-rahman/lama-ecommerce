340bf852f5a243e1a59a789c5ac1ba7c13ac1da9
mahabur-rahman <annur4395@gmail.com>
2022-07-25 09:50:29 +0600

    userSlice implement

--- src/components/Success.js
+++ src/components/Success.js
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 
 const SuccessPayment = () => {
   const location = useLocation();
-  console.log(location);
+  // console.log(location.state.products.products);
 
   return (
     <>
