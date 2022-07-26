e7ca133f947917c089e176205e5758d015edc860
mahabur-rahman <annur4395@gmail.com>
2022-07-26 13:30:19 +0600

    some changes in request methods

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
+const BASE_URL = "http://localhost:4000/api/";
+
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZDdkMmQ5NGQwMWRhZDE4ZDdiMDc3MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1ODU4ODg0NiwiZXhwIjoxNjYxMTgwODQ2fQ.tqmGfayz_TrGbicTxqO3jDpYQ8O06sdDFBQW0utk1ps";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -10,5 +11,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  headers: { token: `Bearer ${TOKEN}` },
+  header: { token: `Bearer ${TOKEN}` },
 });
