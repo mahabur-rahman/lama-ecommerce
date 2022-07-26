05ddd28df2332c1c32cf187daf9c1486184af078
mahabur-rahman <annur4395@gmail.com>
2022-07-26 09:33:40 +0600

    recharts apply

--- admin/src/pages/Home/Home.js
+++ admin/src/pages/Home/Home.js
@@ -3,11 +3,19 @@ import React from "react";
 import "./home.scss";
 // components
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import Charts from "../../components/Chart/Charts";
+import { userData } from "../../Data/data";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Charts
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
