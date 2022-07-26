dc273ac700bdeef758a556b1019df0640664229e
mahabur-rahman <annur4395@gmail.com>
2022-07-26 10:10:17 +0600

    routing added

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,21 +1,52 @@
 // global style
 import "./index.scss";
-
 // components
 import Topbar from "./components/Topbar/Topbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 // pages
 import Home from "./pages/Home/Home";
+// routing
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/UserList/UserList";
+import User from "./pages/User/User";
+import NewUser from "./pages/NewUser/NewUser";
+import { ProductList } from "./pages/ProductList/ProductList";
+import Product from "./pages/Product/Product";
+import NewProduct from "./pages/NewProduct/NewProduct";
 
 function App() {
   return (
     <>
-      <Topbar />
+      <Router>
+        <Topbar />
 
-      <div className="container">
-        <Sidebar />
-        <Home />
-      </div>
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/users">
+              <UserList />
+            </Route>
+            <Route path="/user/:userId">
+              <User />
+            </Route>
+            <Route path="/newUser">
+              <NewUser />
+            </Route>
+            <Route path="/products">
+              <ProductList />
+            </Route>
+            <Route path="/product/:productId">
+              <Product />
+            </Route>
+            <Route path="/newproduct">
+              <NewProduct />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
