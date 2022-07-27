bcb71f0d9dd0afce70240373773bc2873ed85830
mahabur-rahman <annur4395@gmail.com>
2022-07-27 12:09:28 +0600

    featuredInfo get data from API

--- admin/src/components/FeaturedInfo/FeaturedInfo.js
+++ admin/src/components/FeaturedInfo/FeaturedInfo.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 // style
 import "./featuredInfo.scss";
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch {}
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className="box">
       <div className="box_card">
         <h4>Revenue</h4>
-        <span className="mr-3 title">$2, 234</span>
+        <span className="mr-3 title">{income[1]?.total}</span>
         <span>
-          -11.4 <ArrowDownward className="red" />
+          %{Math.floor(perc)}
+          {perc < 0 ? (
+            <ArrowDownward className="featuredIcon negative" />
+          ) : (
+            <ArrowUpward className="featuredIcon" />
+          )}
         </span>
         <pre>Compare to last month</pre>
       </div>
