f39161156f10d80f7a16d05a19e0b5e6952ad7d0
mahabur-rahman <annur4395@gmail.com>
2022-07-28 14:44:24 +0600

    firebase setup start

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdm0S7A_ty5nLbijXmf_GXRMFezjck1XY",
+  authDomain: "lama-ecommerce.firebaseapp.com",
+  projectId: "lama-ecommerce",
+  storageBucket: "lama-ecommerce.appspot.com",
+  messagingSenderId: "926380137566",
+  appId: "1:926380137566:web:43a498387a682e6e84a8cf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
