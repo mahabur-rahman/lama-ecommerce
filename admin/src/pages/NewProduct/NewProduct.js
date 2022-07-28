68e53b05b6803e4afe93b6779c3cee9d45e5a308
mahabur-rahman <annur4395@gmail.com>
2022-07-28 15:15:16 +0600

    new product added

--- admin/src/pages/NewProduct/NewProduct.js
+++ admin/src/pages/NewProduct/NewProduct.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
+// firebase
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import app from "../../firebase";
+
 import "./newProduct.scss";
+import { addProduct } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
 
 export default function NewProduct() {
-  const [inputs, setInputs] = useState({});
-  const [file, setFile] = useState(null);
-  const [cat, setCat] = useState([]);
+  const dispatch = useDispatch();
+
+  const [inputs, setInputs] = useState({}); // title, desc, price, inStock
+  const [file, setFile] = useState(null); // img
+  const [cat, setCat] = useState([]); // categories
 
   // handleChange
   const handleChange = (e) => {
@@ -21,6 +34,50 @@ export default function NewProduct() {
   // handleClick
   const handleClick = (e) => {
     e.preventDefault();
+
+    // generate unique img name
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          // console.log("File available at", downloadURL);
+          // console.log({ ...inputs, img: downloadURL, categories: cat });
+          const product = { ...inputs, img: downloadURL, categories: cat };
+          addProduct(product, dispatch);
+        });
+      }
+    );
   };
 
   return (
